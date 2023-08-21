f184408fc06de651f06cbad2d8731a084cbfdbdc
Arun Kumar Singh <aks.3102@gmail.com>
2023-08-21 17:53:02 +0530

    responsiveness bug fix

--- src/pages/DepositPage/DepositPage.tsx
+++ src/pages/DepositPage/DepositPage.tsx
@@ -154,7 +154,7 @@ function DepositPage(props: any) {
                   <div
                     style={{
                       marginBottom: "35px",
-                      maxWidth: "325px",
+                      width: "325px",
                       display: "flex",
                       flexDirection: "column",
                       alignItems: "center",
